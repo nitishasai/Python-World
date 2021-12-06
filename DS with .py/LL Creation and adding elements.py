6b2f732880579d616f875a26eb14dcdcc0e2b283
Jaya Deekshith <82016666+jayadeekshith@users.noreply.github.com>
2021-12-06 19:49:35 +0530

    Update LL Creation and adding elements.py

--- DS with .py/LL Creation and adding elements.py
+++ DS with .py/LL Creation and adding elements.py
@@ -1,22 +1,28 @@
 class Node:
     def __init__(self,data):
         self.data=data
-        self.next=None             #node creation class
+        self.next=None #node class 
 
 class LinkL:
     def __init__(self):
-        self.head=None              #ll class
+        self.head=None #ll class
+
 
     def addbeg(self, data):
         new_n = Node(data)
+        if self.head is None:
+            self.head = new_n
+            return
         new_n.next = self.head
         self.head = new_n
-
 
 
     def addAft(self,data,x):
         new_n= Node(data)
         n=self.head
+        if n is None:
+            self.head = new_n
+            return
         while(n!=None):
             if x==n.data:
                 break
@@ -26,11 +32,12 @@ class LinkL:
             n.next=new_n
 
 
-
-
     def addbefo(self,data,x):
         new_n=Node(data)
         n=self.head
+        if n is None:
+            self.head = new_n
+            return
         if n.data==x:
             new_n.next=self.head
             self.head=new_n
@@ -44,10 +51,12 @@ class LinkL:
                 n.next=new_n
 
 
-
     def addend(self,data):
         new_n=Node(data)
         n=self.head
+        if n is None:
+            self.head = new_n
+            return
         while(n.next!=None):
             n=n.next
         n.next=new_n
@@ -56,17 +65,15 @@ class LinkL:
 
     def printl(self):
         n=self.head
-        while(n.next!=None):
-            print(n.data,end="->")
+        while(n!=None):
+            print(n.data,end=" ")
             n=n.next
 
 l=list(map(int,input().split()))
 ll=LinkL()
+ll.addbefo(3333,2)
+ll.addAft(333,3)
+
 for i in range(len(l)):
-    if i%2==0:
         ll.addbeg(l[i])
-    else:
-        ll.addend(l[i])
-ll.addAft(33,3)
-ll.addbefo(33,4)
 ll.printl()
